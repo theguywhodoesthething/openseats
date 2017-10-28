14a6766b93bce6ae2a14b95bbb9e50c97f3c4c6e
Jessica Hanson <gmattgmur@gmail.com>
2017-10-28 15:43:15 -0700

    adding multiple date functionality

--- js/openseats.js
+++ js/openseats.js
@@ -1,50 +1,53 @@
-$(document).ready(function() {
-
-  //showPreferredContactMethod()
-  // console.log("LOADED")
-
-});
+// $(document).ready(function() {
+//
+// });
 
 var showPreferredContactMethod = function() {
 
-  console.log("in contact method")
+    var $preferredContactMethod = $("#preferredContactMethod").value;
 
-    var preferredContactMethod = document.getElementById("preferredContactMethod").value;
+    var passengerEmail = '<label for="passengerEmail">Email address</label><input type="email" class="form-control"  placeholder="Enter email">'
+    var passengerTel = '<label for="passengerTel">Phone Number</label><input type="tel" class="form-control" placeholder="Enter phone number">'
 
-    var $passengerEmail = '<label for="passengerEmail">Email address</label><input type="email" class="form-control"  placeholder="Enter email">'
-    var $passengerTel = '<label for="passengerTel">Phone Number</label><input type="tel" class="form-control" placeholder="Enter phone number">'
-
-    if (preferredContactMethod === "Email") {
-      $('#passengerEmail').append($passengerEmail)
+    if ($preferredContactMethod === "Email") {
+      $('#passengerEmail').append(passengerEmail)
       $('#passengerTel').empty()
     } else {
       $('#passengerTel').empty()
       $('#passengerEmail').empty()
-      $('#passengerTel').append($passengerTel)
+      $('#passengerTel').append(passengerTel)
     }
 
 }
 
 var buildPassengerForm = function() {
 
-  // console.log("in buildPassengerForm")
-
     $('#buildPassengerBtn').addClass('active')
     $('#buildOwnerBtn').removeClass('active')
     $('#formDiv').empty();
     $('#formDiv').load('passenger.html', function(){
       showPreferredContactMethod()
+      $( "#datepicker" ).datepicker();
     })
 }
 
 var buildOwnerForm = function() {
-
-  // console.log("in buildOwnerForm")
 
     $('#buildOwnerBtn').addClass('active')
     $('#buildPassengerBtn').removeClass('active')
     $('#formDiv').empty();
     $('#formDiv').load('owner.html', function(){
       showPreferredContactMethod()
+      $( "#datepicker" ).datepicker();
     })
+}
+
+var addNewDate = function() {
+
+  var $newDate = $("#datepicker").value;
+
+  $("#dateDiv").append('<br><p>' + $newDate + '</p>')
+
+  $("datepicker").clearDate();
+
 }
